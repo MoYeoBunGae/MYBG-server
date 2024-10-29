ff365de22c562ac088070c2f4b2ffde3d37d9696
skydreamer <skydreamer210@gmail.com>
2024-10-29 16:15:19 +0900

    feat: EhCache 적용을 위한 의존성 추가 및 Config 작성

---
+++ src/main/java/com/midasdev/mochat/config/cache/CacheConfig.java
@@ -0,0 +1,40 @@
+package com.midasdev.mochat.config.cache;
+
+import com.midasdev.mochat.global.cache.Cache;
+import com.nimbusds.jose.jwk.JWKSet;
+import java.time.Duration;
+import javax.cache.CacheManager;
+import javax.cache.Caching;
+import javax.cache.spi.CachingProvider;
+import org.ehcache.config.CacheConfiguration;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.ExpiryPolicyBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.jsr107.Eh107Configuration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public CacheManager ehCacheManager() {
+        CachingProvider provider = Caching.getCachingProvider();
+        CacheManager cacheManager = provider.getCacheManager();
+
+        CacheConfiguration<String, JWKSet> configuration =
+                CacheConfigurationBuilder.newCacheConfigurationBuilder(
+                                                 String.class,
+                                                 JWKSet.class,
+                                                 ResourcePoolsBuilder.heap(10)
+                                         )
+                                         .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofDays(7)))
+                                         .build();
+
+        cacheManager.createCache(Cache.OIDC_PUBLIC_KEYS, Eh107Configuration.fromEhcacheCacheConfiguration(configuration));
+        return cacheManager;
+    }
+
+}
