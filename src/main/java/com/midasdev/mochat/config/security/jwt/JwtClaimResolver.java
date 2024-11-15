c6b02914b9c023ddb9d6d4895c98e9dac6ce0c7e
skydreamer <skydreamer210@gmail.com>
2024-11-15 16:59:31 +0900

    refactor: 중복된 코드 제거 및 상수처리

--- src/main/java/com/midasdev/mochat/config/security/jwt/JwtClaimResolver.java
+++ src/main/java/com/midasdev/mochat/config/security/jwt/JwtClaimResolver.java
@@ -20,31 +20,6 @@ public class JwtClaimResolver {
 
     private final JwtValidator jwtValidator;
 
-    public String extractValueWithoutValidation(String token, String key) {
-        // REFACTOR: extractValueWithoutValidation 로 바꾸기
-        return extractFromHeader(token, key);
-    }
-
-    private String extractFromHeader(String token, String key) {
-        // REFACTOR: extractValueWithoutValidation 로 바꾸기
-        String headerToken = token.split("\\.")[0];
-        byte[] decodedHeader = Base64.getDecoder().decode(headerToken);
-        Map<String, Object> headerData;
-        try {
-            headerData = new ObjectMapper().readValue(decodedHeader, new TypeReference<>() {
-            });
-        } catch (IOException e) {
-            throw new ApplicationException(ApplicationExceptionType.JWT_PARSING_EXCEPTION, "HEADER", key);
-        }
-
-        return headerData.get(key).toString();
-    }
-
-    public String extractValue(String token, TokenType tokenType, String key) {
-        Jws<Claims> claimsJws = jwtValidator.validate(token, tokenType);
-        return getFromClaim(claimsJws, key);
-    }
-
     public String extractValueWithoutValidation(String token, String key, JwtComponent jwtComponent) {
         String tokenComponent = token.split("\\.")[jwtComponent.getIndex()];
         byte[] decodedComponent = Base64.getDecoder().decode(tokenComponent);
@@ -59,6 +34,11 @@ public class JwtClaimResolver {
                  .setValidation(jwtComponentData::containsKey)
                  .validateOrThrow(() -> new ApplicationException(ApplicationExceptionType.JWT_CLAIMS_KEY_NOT_FOUND, key));
         return jwtComponentData.get(key).toString();
+    }
+
+    public String extractValue(String token, TokenType tokenType, String key) {
+        Jws<Claims> claimsJws = jwtValidator.validate(token, tokenType);
+        return getFromClaim(claimsJws, key);
     }
 
     public String getFromClaim(Jws<Claims> claimsJws, String key) {
