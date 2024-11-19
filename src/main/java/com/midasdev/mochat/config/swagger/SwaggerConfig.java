4d424797cfeb8b4d1a4140bed7f6af4ad1f18587
skydreamer <skydreamer210@gmail.com>
2024-11-19 18:05:55 +0900

    fix: 모든 API에 인증 설정 들어가지 않도록 수정

--- src/main/java/com/midasdev/mochat/config/swagger/SwaggerConfig.java
+++ src/main/java/com/midasdev/mochat/config/swagger/SwaggerConfig.java
@@ -3,20 +3,19 @@ package com.midasdev.mochat.config.swagger;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
-        info = @Info(title = "Mochat API", version = "1.0"),
-        security = @SecurityRequirement(name = "BearerAuth")
+        info = @Info(title = "Mochat API", version = "1.0")
 )
 @SecurityScheme(
         name = "BearerAuth",
         type = SecuritySchemeType.HTTP,
         scheme = "bearer",
-        bearerFormat = "JWT"
+        bearerFormat = "JWT",
+        description = "Token Authentication"
 )
 public class SwaggerConfig {
 
