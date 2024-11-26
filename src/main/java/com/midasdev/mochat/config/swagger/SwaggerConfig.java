c86927fe9eac8790a2e09cc0fe75638dfe4fd262
skydreamer <skydreamer210@gmail.com>
2024-11-26 17:58:15 +0900

    fix: swagger 요청 서버 리스트 수정

--- src/main/java/com/midasdev/mochat/config/swagger/SwaggerConfig.java
+++ src/main/java/com/midasdev/mochat/config/swagger/SwaggerConfig.java
@@ -4,16 +4,15 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
-        info = @Info(title = "Mochat API", version = "1.0"),
-        servers = {
-                @Server(url = "https://app.testsvc-avl87.store", description = "mochat dev 서버"),
-                @Server(url = "http://localhost:8080", description = "mochat local 서버")
-        }
+        info = @Info(title = "Mochat API", version = "1.0")
 )
 @SecurityScheme(
         name = "BearerAuth",
@@ -23,5 +22,17 @@ import org.springframework.context.annotation.Configuration;
         description = "Token Authentication"
 )
 public class SwaggerConfig {
+
+    @Value("${server.url}")
+    private String serverUrl;
+
+    @Bean
+    public OpenAPI openAPI() {
+        Server server = new Server();
+        server.setUrl(serverUrl);
+
+        return new OpenAPI()
+                .addServersItem(server);
+    }
 
 }
