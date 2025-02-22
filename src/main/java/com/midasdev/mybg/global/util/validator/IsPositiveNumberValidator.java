c0e46dba4f526c808df1ba645073fe4311b35ee3
skydreamer <skydreamer210@gmail.com>
2025-02-22 22:58:46 +0900

    :sparkles: feat: IsPositiveNumber custom validator 구현

---
+++ src/main/java/com/midasdev/mybg/global/util/validator/IsPositiveNumberValidator.java
@@ -0,0 +1,13 @@
+package com.midasdev.mybg.global.util.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class IsPositiveNumberValidator implements ConstraintValidator<IsPositiveNumber, Number> {
+
+    @Override
+    public boolean isValid(Number value, ConstraintValidatorContext context) {
+        return value != null && value.longValue() > 0;
+    }
+
+}
