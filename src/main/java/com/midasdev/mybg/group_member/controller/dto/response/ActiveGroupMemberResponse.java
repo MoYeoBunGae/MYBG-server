857987c085aa6fd3d9dbfc0e4a1c888a599d65f4
skydreamer <skydreamer210@gmail.com>
2025-02-23 19:31:19 +0900

    :bug: fix: 그룹 참여 응답 수정

--- src/main/java/com/midasdev/mybg/group_member/controller/dto/response/ActiveGroupMemberResponse.java
+++ src/main/java/com/midasdev/mybg/group_member/controller/dto/response/ActiveGroupMemberResponse.java
@@ -1,9 +1,6 @@
 package com.midasdev.mybg.group_member.controller.dto.response;
 
-import com.midasdev.mybg.global.audit.Audit;
-import com.midasdev.mybg.group.domain.Group;
 import com.midasdev.mybg.group_member.domain.GroupMember;
-import com.midasdev.mybg.member.domain.Member;
 import java.time.LocalDateTime;
 import lombok.Builder;
 
@@ -12,8 +9,8 @@ public record ActiveGroupMemberResponse(
         Long id,
         String nickname,
         LocalDateTime createdAt,
-        Member member,
-        Group group
+        Long memberId,
+        Long groupId
 ) {
 
     public static ActiveGroupMemberResponse from(GroupMember groupMember) {
@@ -21,8 +18,9 @@ public record ActiveGroupMemberResponse(
                                         .id(groupMember.getId())
                                         .nickname(groupMember.getNickname())
                                         .createdAt(groupMember.getAudit().getCreatedAt())
-                                        .member(groupMember.getMember())
-                                        .group(groupMember.getGroup())
+                                        .memberId(groupMember.getMember().getId())
+                                        .groupId(groupMember.getGroup().getId())
                                         .build();
     }
+
 }
