f7bb2f101c09e844ab0f7e4e50792565adbca6f1
skydreamer <skydreamer210@gmail.com>
2025-02-25 18:40:04 +0900

    :sparkles: feat: 참여 중인 그룹 API 서비스 로직 구현

--- src/main/java/com/midasdev/mybg/group/repository/GroupRepository.java
+++ src/main/java/com/midasdev/mybg/group/repository/GroupRepository.java
@@ -1,8 +1,11 @@
 package com.midasdev.mybg.group.repository;
 
 import com.midasdev.mybg.group.domain.Group;
+import java.util.List;
 import java.util.Optional;
 
 public interface GroupRepository {
     Optional<Group> findById(Long groupId);
+
+    List<Group> findGroupsByMemberId(Long memberId);
 }
--- src/main/java/com/midasdev/mybg/group/repository/GroupRepositoryImpl.java
+++ src/main/java/com/midasdev/mybg/group/repository/GroupRepositoryImpl.java
@@ -2,8 +2,10 @@ package com.midasdev.mybg.group.repository;
 
 import com.midasdev.mybg.group.domain.Group;
 import com.midasdev.mybg.group.domain.QGroup;
+import com.midasdev.mybg.group_member.domain.QGroupMember;
 import com.midasdev.mybg.member.domain.QMember;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -26,5 +28,21 @@ public class GroupRepositoryImpl implements GroupRepository {
                                       .fetchOne();
 
         return Optional.ofNullable(result);
+    }
+
+    @Override
+    public List<Group> findGroupsByMemberId(Long memberId) {
+        QGroup group = QGroup.group;
+        QGroupMember groupMember = QGroupMember.groupMember;
+        QMember member = QMember.member;
+
+        return jpaQueryFactory.selectFrom(group)
+                .join(groupMember).on(group.eq(groupMember.group))
+                .fetchJoin()
+                .join(group.owner, member)
+                .fetchJoin()
+                .where(groupMember.member.id.eq(memberId))
+                .fetch()
+                .stream().toList();
     }
 }
