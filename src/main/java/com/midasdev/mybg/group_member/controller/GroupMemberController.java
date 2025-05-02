22d355eb2780fdfe930b9e5ed82bc3765ef8a257
skydreamer <skydreamer210@gmail.com>
2025-05-02 22:32:01 +0900

    :sparkles: feat: 그룹 참여 정보 수정 API 컨트롤러 작성

--- src/main/java/com/midasdev/mybg/group_member/controller/GroupMemberController.java
+++ src/main/java/com/midasdev/mybg/group_member/controller/GroupMemberController.java
@@ -3,6 +3,7 @@ package com.midasdev.mybg.group_member.controller;
 import static com.midasdev.mybg.config.swagger.SwaggerConfig.SECURITY_SCHEME_NAME;
 
 import com.midasdev.mybg.group_member.controller.dto.request.GroupJoinRequest;
+import com.midasdev.mybg.group_member.controller.dto.request.GroupMemberProfileUpdateRequest;
 import com.midasdev.mybg.group_member.controller.dto.response.ActiveGroupMemberResponse;
 import com.midasdev.mybg.group_member.domain.GroupMember;
 import com.midasdev.mybg.group_member.service.GroupMemberService;
@@ -12,9 +13,12 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +39,20 @@ public class GroupMemberController {
                                                                @Valid @RequestBody GroupJoinRequest groupJoinRequest) {
         GroupMember groupMember = groupMemberService.joinGroup(member, groupJoinRequest);
         return ResponseEntity.ok(ActiveGroupMemberResponse.from(groupMember));
+    }
+
+    @Operation(
+            summary = "그룹 멤버 프로필 수정 API",
+            description = "그룹에 참여 중인 사용자의 닉네임과 프로필 이미지를 수정합니다.",
+            security = @SecurityRequirement(name = SECURITY_SCHEME_NAME)
+    )
+    @PatchMapping(value = "/profile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ActiveGroupMemberResponse> updateGroupMemberProfile(
+            @AuthenticationPrincipal Member member,
+            @Valid @ModelAttribute GroupMemberProfileUpdateRequest request
+    ) {
+        GroupMember updatedMember = groupMemberService.updateProfile(member, request);
+        return ResponseEntity.ok(ActiveGroupMemberResponse.from(updatedMember));
     }
 
 }
