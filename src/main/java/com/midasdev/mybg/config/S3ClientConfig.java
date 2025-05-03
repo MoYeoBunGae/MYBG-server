86bddd9960fb2b34e87d3524098759599f0c90b9
skydreamer <skydreamer210@gmail.com>
2025-05-03 16:52:52 +0900

    :bug: fix: S3ClientConfig 수정

--- src/main/java/com/midasdev/mybg/config/S3ClientConfig.java
+++ src/main/java/com/midasdev/mybg/config/S3ClientConfig.java
@@ -1,19 +1,29 @@
 package com.midasdev.mybg.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3ClientConfig {
 
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
                        .region(Region.AP_NORTHEAST_2)
-                       .credentialsProvider(DefaultCredentialsProvider.create())
+                       .credentialsProvider(StaticCredentialsProvider.create(credentials))
                        .build();
     }
 
