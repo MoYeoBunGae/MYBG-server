1d4dbe1dfc1d6c2bb4300c5c6fcc7f2433279d9a
skydreamer <skydreamer210@gmail.com>
2025-06-12 22:52:59 +0900

    feat: 번개 생성 서비스 - 번개 및 날짜 후보 저장

---
+++ src/main/java/com/midasdev/mybg/bungae/repository/BungaeRecruitDateOptionRepository.java
@@ -0,0 +1,7 @@
+package com.midasdev.mybg.bungae.repository;
+
+import com.midasdev.mybg.bungae.domain.BungaeRecruitDateOption;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BungaeRecruitDateOptionRepository extends JpaRepository<BungaeRecruitDateOption, Long> {
+}
--- src/main/java/com/midasdev/mybg/bungae/service/BungaeService.java
+++ src/main/java/com/midasdev/mybg/bungae/service/BungaeService.java
@@ -3,20 +3,27 @@ package com.midasdev.mybg.bungae.service;
 import com.midasdev.mybg.bungae.controller.dto.request.BungaeCreateRequest;
 import com.midasdev.mybg.bungae.domain.Bungae;
 import com.midasdev.mybg.bungae.domain.BungaeDateTime;
+import com.midasdev.mybg.bungae.domain.BungaeRecruitDateOption;
 import com.midasdev.mybg.bungae.domain.BungaeStatus;
+import com.midasdev.mybg.bungae.repository.BungaeRecruitDateOptionRepository;
+import com.midasdev.mybg.bungae.repository.BungaeRepository;
 import com.midasdev.mybg.group_member.domain.GroupMember;
 import com.midasdev.mybg.group_member.repository.GroupMemberRepository;
 import com.midasdev.mybg.global.exception.ApplicationException;
 import com.midasdev.mybg.global.exception.ApplicationExceptionType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class BungaeService {
 
     private final GroupMemberRepository groupMemberRepository;
+    private final BungaeRepository bungaeRepository;
+    private final BungaeRecruitDateOptionRepository bungaeRecruitDateOptionRepository;
 
+    @Transactional
     public Bungae createBungae(BungaeCreateRequest request) {
         // 1. groupId와 groupMemberId로 삭제되지 않은 groupMember 조회 (그룹에 속하는 그룹멤버가 없다면 예외)
         GroupMember hostGroupMember = groupMemberRepository.findByIdAndGroupIdAndDeletedFalse(request.hostGroupMemberId(), request.groupId())
@@ -58,8 +65,19 @@ public class BungaeService {
                 .build();
 
         // 3. Bungae 엔티티 저장
+        Bungae savedBungae = bungaeRepository.save(bungae);
 
         // 4. 날짜 후보가 여러개라면 날짜 후보 저장 (BungaeRecruitDateOption)
+        if (!request.hasSingleDateCandidate()) {
+            request.dateCandidates().forEach(date -> {
+                BungaeRecruitDateOption option = BungaeRecruitDateOption.builder()
+                        .dateOption(date)
+                        .voteCount(0)
+                        .bungae(savedBungae)
+                        .build();
+                bungaeRecruitDateOptionRepository.save(option);
+            });
+        }
 
         // 5. Bungae 참여자에 host GroupMember 추가
 
