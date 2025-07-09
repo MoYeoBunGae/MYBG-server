6cc9b9ce8c0ee2aefac3b59beedef8965e5b9b08
skydreamer <skydreamer210@gmail.com>
2025-07-09 23:55:13 +0900

    feat: dto의 기본값 매핑을 위한 로직 구현

---
+++ src/main/java/com/midasdev/mybg/global/util/default_value_mapper/Default.java
@@ -0,0 +1,13 @@
+package com.midasdev.mybg.global.util.default_value_mapper;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Default {
+    String value();
+}
+
---
+++ src/main/java/com/midasdev/mybg/global/util/default_value_mapper/DefaultedRequestBodyResolver.java
@@ -0,0 +1,41 @@
+package com.midasdev.mybg.global.util.default_value_mapper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.stream.Collectors;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class DefaultedRequestBodyResolver implements HandlerMethodArgumentResolver {
+
+    private final ObjectMapper objectMapper;
+
+    public DefaultedRequestBodyResolver(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(DefaultedRequestBody.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) throws Exception {
+
+        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+        String json = servletRequest.getReader().lines().collect(Collectors.joining());
+
+        // TODO: 발생한 예외가 어디서 잡히는 지 확인
+        // TODO: empty body 일 경우 어떻게 되는지 확인하고, 적절한 처리
+
+        Object dto = objectMapper.readValue(json, parameter.getParameterType());
+        DefaultValueResolver.applyDefaults(dto);
+        return dto;
+    }
+}
