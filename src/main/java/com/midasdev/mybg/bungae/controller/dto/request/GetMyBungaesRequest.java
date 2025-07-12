8aca5cc00e38d3fd2582f564606876c6c6e60882
skydreamer <skydreamer210@gmail.com>
2025-07-12 22:58:58 +0900

    fix: GetMyBungaesRequest를 CursorPageRequest 상속하도록 수정

--- src/main/java/com/midasdev/mybg/bungae/controller/dto/request/GetMyBungaesRequest.java
+++ src/main/java/com/midasdev/mybg/bungae/controller/dto/request/GetMyBungaesRequest.java
@@ -1,19 +1,25 @@
 package com.midasdev.mybg.bungae.controller.dto.request;
 
 import com.midasdev.mybg.bungae.domain.BungaeStatus;
+import com.midasdev.mybg.global.util.cursor_page.CursorPageRequest;
 import io.swagger.v3.oas.annotations.media.Schema;
-
 import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @Schema(description = "내 번개모임 목록 조회 요청 DTO")
-public record GetMyBungaesRequest(
-        @Schema(
-                title = "번개 상태",
-                description = "조회할 번개 상태값 리스트",
-                example = "[\"RECRUITING\", \"DATE_VOTING\"]",
-                allowableValues = {"RECRUITING", "RECRUITING_CLOSED", "DATE_VOTING", "CLOSED", "CANCELED"},
-                requiredMode = RequiredMode.NOT_REQUIRED
-        )
-        List<BungaeStatus> statuses
-) {}
+@Getter
+@AllArgsConstructor
+public class GetMyBungaesRequest extends CursorPageRequest {
+
+    @Schema(
+            title = "번개 상태",
+            description = "조회할 번개 상태값 리스트",
+            example = "[\"RECRUITING\", \"DATE_VOTING\"]",
+            allowableValues = {"RECRUITING", "RECRUITING_CLOSED", "DATE_VOTING", "CLOSED", "CANCELED"},
+            requiredMode = RequiredMode.NOT_REQUIRED
+    )
+    private List<BungaeStatus> statuses;
+
+}
