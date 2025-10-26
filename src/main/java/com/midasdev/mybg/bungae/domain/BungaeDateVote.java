212513a919a018b8e20520c51f1279b55ecf4fea
skydreamer <skydreamer210@gmail.com>
2025-10-26 12:21:13 +0900

    fix: BungaeDateVote Persistable 인터페이스 구현
    save시 추가 select 쿼리 방지

--- src/main/java/com/midasdev/mybg/bungae/domain/BungaeDateVote.java
+++ src/main/java/com/midasdev/mybg/bungae/domain/BungaeDateVote.java
@@ -7,17 +7,21 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
+import jakarta.persistence.PostLoad;
+import jakarta.persistence.PostPersist;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Persistable;
 
 @Getter
 @Entity
 @Builder
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class BungaeDateVote {
+public class BungaeDateVote implements Persistable<BungaeDateVoteId> {
 
     @EmbeddedId
     private BungaeDateVoteId id;
@@ -31,4 +35,16 @@ public class BungaeDateVote {
     @MapsId("dateOptionId")
     @JoinColumn(name = "bungae_recruit_date_option_id", nullable = false)
     private BungaeRecruitDateOption dateOption;
+
+    // transient flag to indicate whether this entity is new. Default true for newly built instances.
+    @Transient
+    @Builder.Default
+    private boolean isNew = true;
+
+    @PostPersist
+    @PostLoad
+    private void markNotNew() {
+        this.isNew = false;
+    }
+
 }
